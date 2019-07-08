6dff5d49f2cf038fa63d0712759df38b1f38d25f
3-zero <seyoungkim0717@gmail.com>
2019-07-08 16:17:33 +0900

    Default Changelist

---
+++ Equipment/Cutter.py
@@ -0,0 +1,34 @@
+import random
+
+class Cutter:
+    def __init__(self, env, allocator, num):
+        self.env = env
+        self.alloc = allocator
+
+        self.name = 'cutter_' + str(num + 1)
+
+        self.current_job = None
+        print(self.name + ' :: created')
+
+    def calc_cut_time(self):
+        print(self.name, ' :: calculate cut time')
+        return random.randint(30, 50)
+
+    def run(self):
+        while True:
+            self.current_job = self.alloc.get_next_cut_job()
+            if self.current_job == None:
+                yield self.env.timeout(10)
+                continue
+            print(self.env.now, self.name, ':: cut start', self.current_job)
+            cut_time = self.calc_cut_time()
+            self.current_job['properties']['current_equip'] = self.name
+            self.current_job['properties']['last_process'] = 'cut'
+            self.current_job['properties']['last_process_end_time'] = self.env.now + cut_time
+            yield self.env.timeout(cut_time)
+            self.current_job['properties']['next_instruction'] += 1
+            if len(self.current_job['properties']['instruction_list'][0]) == self.current_job['properties']['next_instruction']:
+                self.current_job['properties']['state'] = 'done'
+            print(self.env.now, self.name, ':: cut end', self.current_job)
+
+            self.alloc.end_job(self.current_job)
